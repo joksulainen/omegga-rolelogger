4475f3d0e823015fb4032f14a46da6716e7af6e3
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-07-18 16:53:03 +0300

    use shared timestamp variable

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -4,13 +4,13 @@ import fs from 'fs';
 type Config = {};
 type Storage = {};
 
-const dateRegEx = /^\[(?<date>\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3})\]\[\s*\d+\]/;
-const roleURegEx = /^\[(?<date>\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3})\]\[\s*\d+\]LogChat: (?<log>(?<target>[^:]+?)(?<targetp> \(not present\))? (?:has become|is no longer) (?<role>.+) \((?<action>granted|revoked) by (?<actor>.+)\))$/;
-const roleMRegEx = /^\[(?<date>\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3})\]\[\s*\d+\]LogChat: (?<log>(?<actor>[^:]+?) (?<action>created|updated|removed) the (?<role>.+) role\.)$/;
+const tsRegEx = /^\[(\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3})\]\[\s*\d+\]/;
+const roleURegEx = /^\[\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3}\]\[\s*\d+\]LogChat: (?<log>(?<target>[^:]+?)(?<targetp> \(not present\))? (?:has become|is no longer) (?<role>.+) \((?<action>granted|revoked) by (?<actor>.+)\))$/;
+const roleMRegEx = /^\[\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3}\]\[\s*\d+\]LogChat: (?<log>(?<actor>[^:]+?) (?<action>created|updated|removed) the (?<role>.+) role\.)$/;
 const logFolder = './logs/roles/';
 
 function validateLogFolder() {
-  if (fs.existsSync(logFolder)) {return;}
+  if (fs.existsSync(logFolder)) { return; }
   fs.mkdir(logFolder, { recursive: true }, (err) => {
     if (!err) {
       console.log("Created log folder");
@@ -33,15 +33,19 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     validateLogFolder();
     
     this.omegga.on('line', (logLine: string) => {
+      const matchTS = logLine.match(tsRegEx)
+      if (!matchTS) { return; }
+      const ts = matchTS[1]
+      
       const matchU = logLine.match(roleURegEx);
       if (matchU) {
-        fs.appendFileSync(logFolder + `${matchU.groups.date.substring(0, 10)}.log`, `[${matchU.groups.date}] ${matchU.groups.log}\n`);
+        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, `[${ts}] ${matchU.groups.log}\n`);
         return;
       }
       
       const matchM = logLine.match(roleMRegEx);
       if (matchM) {
-        fs.appendFileSync(logFolder + `${matchM.groups.date.substring(0, 10)}.log`, `[${matchM.groups.date}] ${matchM.groups.log}\n`);
+        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, `[${ts}] ${matchM.groups.log}\n`);
       }
     });
     
