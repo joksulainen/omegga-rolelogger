000833522261468112cfc9b61cfc398a3147d268
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-07-19 06:30:35 +0300

    added usernames to log

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,4 +1,4 @@
-import OmeggaPlugin, { OL, PS, PC } from 'omegga';
+import { OmeggaPlugin, OL, PS, PC, OmeggaPlayer } from 'omegga';
 import fs from 'fs';
 
 
@@ -11,20 +11,44 @@ const tsRegEx = /^\[(\d{4}\.\d\d\.\d\d-\d\d\.\d\d\.\d\d:\d{3})\]\[\s*\d+\]/;
 
 // [2025.07.17-07.48.43:644][996]LogChat: joksulainen has become Moderator (granted by joksulainen)
 // [2025.07.17-07.48.46:779][179]LogChat: joksulainen (not present) is no longer Moderator (revoked by joksulainen)
-const rUserRegEx = /^.+LogChat: (?<log>(?<target>[^:]+?)(?: \(not present\))? (?:has become|is no longer) (?<role>.+) \((?<action>granted|revoked) by (?<actor>.+)\))$/;
+const rUserRegEx = /^.+LogChat: (?<target>[^:]+?)(?<targetp> \(not present\))? (?<actiont>has become|is no longer) (?<role>.+) \((?<action>granted|revoked) by (?<actor>.+)\)$/;
 
 // [2025.07.17-10.09.56:565][824]LogChat: joksulainen created the New Role 1 role.
 // [2025.07.17-10.10.13:141][842]LogChat: joksulainen updated the New Role role.
 // [2025.07.17-10.09.16:679][453]LogChat: joksulainen removed the New Role 0 role.
-const roleManageRegEx = /^.+LogChat: (?<log>(?<actor>[^:]+?) (?<action>created|updated|removed) the (?<role>.+) role\.)$/;
+const rManageRegEx = /^.+LogChat: (?<actor>[^:]+?) (?<action>created|updated|removed) the (?<role>.+) role\.$/;
 
 function validateLogFolder() {
   if (fs.existsSync(logFolder)) return;
   fs.mkdir(logFolder, { recursive: true }, err => {
     if (!err) {
-      console.log("Created log folder");
+      console.log('Created log folder');
     }
   });
+}
+
+function findPlayersByExactDisplayName(name: string): OmeggaPlayer[] {
+  let result: OmeggaPlayer[] = Array<OmeggaPlayer>();
+  
+  for (const player of Omegga.players) {
+    if (player.displayName !== name) continue;
+    result.push(player);
+  }
+  
+  return result;
+}
+
+function playerArrayToString(array: OmeggaPlayer[]): string {
+  if (array.length === 0) return '[]';
+  
+  let string = '[';
+  
+  for (const player of array) {
+    string += player.name + ', ';
+  }
+  
+  string = string.substring(0, string.lastIndexOf(', ')) + ']';
+  return string;
 }
 
 export default class Plugin implements OmeggaPlugin<Config, Storage> {
@@ -42,19 +66,50 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     validateLogFolder();
     
     this.omegga.on('line', logLine => {
-      const matchTS = logLine.match(tsRegEx)
-      if (!matchTS) return;
-      const ts = matchTS[1]
+      const matchTimestamp = logLine.match(tsRegEx);
+      if (!matchTimestamp) return;
+      const ts = matchTimestamp[1];
       
-      const matchU = logLine.match(rUserRegEx);
-      if (matchU) {
-        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, `[${ts}] ${matchU.groups.log}\n`);
+      const matchUser = logLine.match(rUserRegEx);
+      if (matchUser) {
+        const { target, targetp, actiont, role, action, actor } = matchUser.groups;
+        
+        let targetNames: string = undefined;
+        
+        if (!targetp) {
+          const targetPlayers = findPlayersByExactDisplayName(target);
+          targetNames = playerArrayToString(targetPlayers);
+        }
+        
+        const actorPlayers = findPlayersByExactDisplayName(actor);
+        
+        // uncomment when missing permissions for default roles get inserted in getPermissions()
+        // actorPlayers.filter((player, _, __) => {
+        //   return player.getPermissions()['BR.Permission.GrantRoles'];
+        // });
+        
+        const actorNames: string = actorPlayers.length !== 0 ? playerArrayToString(actorPlayers) : '[SERVER]';
+        
+        const log = `[${ts}] ${target}${targetp ?? ' ' + targetNames} ${actiont} ${role} (${action} by ${actor}${actorNames ? ' ' + actorNames : ''})\n`;
+        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, log);
         return;
       }
       
-      const matchM = logLine.match(roleManageRegEx);
-      if (matchM) {
-        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, `[${ts}] ${matchM.groups.log}\n`);
+      const matchManage = logLine.match(rManageRegEx);
+      if (matchManage) {
+        const { actor, action, role } = matchManage.groups;
+        
+        const actorPlayers = findPlayersByExactDisplayName(actor);
+        
+        // uncomment when missing permissions for default roles get inserted in getPermissions()
+        // actorPlayers.filter((player, _, __) => {
+        //   return player.getPermissions()['BR.Permission.EditRoles'];
+        // });
+        
+        const actorNames: string = actorPlayers.length !== 0 ? playerArrayToString(actorPlayers) : '[SERVER]';
+        
+        const log = `[${ts}] ${actor}${actorNames ? ' ' + actorNames : ''} ${action} the ${role} role\n`;
+        fs.appendFileSync(logFolder + `${ts.substring(0, 10)}.log`, log);
       }
     });
     
